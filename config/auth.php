f9bffc51ad0e1e9c88f04c75a7582d6ad28286fa
nb-diar <diar@nusantarabetastudio.com>
2019-02-10 15:37:22 +0700

    change users model

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => Bromo\Auth\Models\Admin::class,
         ],
 
         // 'users' => [
