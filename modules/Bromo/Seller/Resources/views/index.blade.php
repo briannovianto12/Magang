3c82e0b8352ef7355eea638e75a9bd911462515d
Dinan Riqal <dinan.riqal@gmail.com>
2019-02-11 16:03:02 +0700

    fix wording and hide button
    Took 3 minutes

--- modules/Bromo/Seller/Resources/views/index.blade.php
+++ modules/Bromo/Seller/Resources/views/index.blade.php
@@ -17,22 +17,22 @@
             <div class="m-portlet__head-caption">
                 <div class="m-portlet__head-title">
                     <h3 class="m-portlet__head-text">
-                        Seller
+                        List of Seller
                     </h3>
                 </div>
             </div>
-            <div class="m-portlet__head-tools">
+            {{--<div class="m-portlet__head-tools">
                 <ul class="m-portlet__nav">
                     <li class="m-portlet__nav-item">
                         <a href="#" class="btn btn-primary m-btn m-btn--pill m-btn--custom m-btn--icon m-btn--air">
 						<span>
 							<i class="la la-cart-plus"></i>
-							<span>New Record</span>
+							<span>Add Seller</span>
 						</span>
                         </a>
                     </li>
                 </ul>
-            </div>
+            </div>--}}
         </div>
         <div class="m-portlet__body">
             <!--begin: Datatable -->
