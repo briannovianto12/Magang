88019184a1bd966313b9f70cedc9bae69a3884f7
Dinan Riqal <dinan.riqal@gmail.com>
2019-02-13 14:39:02 +0700

    Add list seller
    Took 1 hour 19 minutes

--- modules/Bromo/Seller/Http/Controllers/SellerController.php
+++ modules/Bromo/Seller/Http/Controllers/SellerController.php
@@ -2,25 +2,37 @@
 
 namespace Bromo\Seller\Http\Controllers;
 
+use Bromo\Seller\DataTables\SellerDataTable;
+use Bromo\Seller\Models\Shop;
 use Illuminate\Routing\Controller;
 
 class SellerController extends Controller
 {
+    protected $module;
+    protected $model;
+    protected $title;
 
     /**
      * Create a new controller instance.
      *
-     * @return void
+     * @param Shop $shop
      */
-    public function __construct()
+    public function __construct(Shop $shop)
     {
-//        $this->middleware('auth');
+        $this->model = $shop;
+        $this->module = 'seller';
+        $this->title = ucwords($this->module);
     }
 
-    public function index()
+    public function index(SellerDataTable $dataTable)
     {
-        $data['title'] = 'Seller';
+        $data['title'] = $this->title;
 
-        return view('seller::index', $data);
+        return $dataTable
+            ->with([
+                'module' => $this->module,
+                'model' => $this->model
+            ])
+            ->render("{$this->module}::list", $data);
     }
 }
