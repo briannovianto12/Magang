9b14bf91b240b33191204fe988662bd60a9dd80f
Dinan Riqal <dinan.riqal@gmail.com>
2019-02-21 11:52:08 +0700

    Add Models Product Attribute.
    Took 36 minutes

---
+++ modules/Bromo/Product/Models/ProductAttributeValueOption.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Bromo\Product\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Modules\Nbs\BaseResource\Traits\SnowFlakeTrait;
+use Nbs\Theme\Utils\FormatDates;
+use Nbs\Theme\Utils\TimezoneAccessor;
+
+class ProductAttributeValueOption extends Model
+{
+    use FormatDates,
+        TimezoneAccessor,
+        SnowFlakeTrait;
+
+    const CREATED_AT = null;
+
+    protected $table = 'product_attribute_value_option';
+
+    protected $fillable = [
+        'key',
+        'value',
+        'sku_code'
+    ];
+
+}
