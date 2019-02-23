b95182724df606c741e2780c593852e6baba3afa
nb-diar <diar@nusantarabetastudio.com>
2019-02-23 19:36:47 +0700

    add const status

--- modules/Bromo/Product/Models/ProductStatus.php
+++ modules/Bromo/Product/Models/ProductStatus.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductStatus extends Model
 {
+    const SUBMIT = 1;
+    const APPROVE = 2;
+    const REJECT = 3;
+
     const CREATED_AT = null;
     const UPDATED_AT = null;
     protected $table = 'product_status';
