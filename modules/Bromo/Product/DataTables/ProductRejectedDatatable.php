08b1d933d3ef74278c815b38456a6fc1b48e0839
nb-diar <diar@nusantarabetastudio.com>
2019-02-25 12:27:41 +0700

    add rejected product datatable

---
+++ modules/Bromo/Product/DataTables/ProductRejectedDatatable.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Bromo\Product\DataTables;
+
+use Bromo\Product\Models\ProductStatus;
+use Yajra\DataTables\Services\DataTable;
+
+class ProductRejectedDatatable extends DataTable
+{
+    public function ajax()
+    {
+        return datatables($this->query())
+            ->addIndexColumn()
+            ->editColumn('updated_at', function ($data) {
+                return $data->updated_at_formatted;
+            })
+            ->addColumn('action', function ($data) {
+                $action = [
+                    'show_url' => route("{$this->module}.show", $data->id),
+                    'id' => $data->id
+                ];
+
+                return view('layouts.includes.actions', $action);
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+
+    public function query()
+    {
+        $query = $this->model->select([
+            'product.id',
+            'product.ext_id',
+            'product.name',
+            'product.unit_type',
+            'product.display_price',
+            'product.product_type_id',
+            'product.updated_at',
+
+            'shop.name as shop_name',
+            'product_status.name as status'
+        ])->join('shop', 'shop.id', '=', 'product.shop_id')
+            ->join('product_status', 'product_status.id', '=', 'product.status')
+            ->where('product.status', ProductStatus::REJECT);
+
+        return $this->applyScopes($query);
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return $this->module . "_" . time();
+    }
+}
