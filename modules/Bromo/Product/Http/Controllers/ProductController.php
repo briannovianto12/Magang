69d414820b4bc52f13b986d0af27355daefc2a07
nb-diar <diar@nusantarabetastudio.com>
2019-03-04 16:00:04 +0700

    add approved table in product controller

--- modules/Bromo/Product/Http/Controllers/ProductController.php
+++ modules/Bromo/Product/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace Bromo\Product\Http\Controllers;
 
+use Bromo\Product\DataTables\ProductApprovedDatatable;
 use Bromo\Product\DataTables\ProductRejectedDatatable;
 use Bromo\Product\DataTables\ProductSubmitedDatatable;
 use Bromo\Product\Models\Product;
@@ -11,9 +12,7 @@ class ProductController extends Controller
 {
 
     protected $module;
-
     protected $model;
-
     protected $title;
 
     /**
@@ -29,6 +28,15 @@ class ProductController extends Controller
         $this->middleware('auth');
     }
 
+    public function show($id)
+    {
+        $data['module'] = $this->module;
+        $data['title'] = $this->title;
+        $data['data'] = Product::findOrFail($id);
+
+        return view("{$this->module}::detail", $data);
+    }
+
     public function index()
     {
         $data['module'] = $this->module;
@@ -40,14 +48,30 @@ class ProductController extends Controller
     public function submited(ProductSubmitedDatatable $datatable)
     {
         return $datatable
-            ->with('model', $this->model)
+            ->with([
+                'model' => $this->model,
+                'module' => $this->module,
+            ])
             ->render("$this->module::list");
     }
 
     public function rejected(ProductRejectedDatatable $datatable)
     {
         return $datatable
-            ->with('model', $this->model)
+            ->with([
+                'model' => $this->model,
+                'module' => $this->module,
+            ])
+            ->render("$this->module::list");
+    }
+
+    public function approved(ProductApprovedDatatable $datatable)
+    {
+        return $datatable
+            ->with([
+                'model' => $this->model,
+                'module' => $this->module,
+            ])
             ->render("$this->module::list");
     }
 }
