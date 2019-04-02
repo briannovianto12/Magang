9c99ae65f1dc3c2fc7a80e9f8d5e71dc28d0bf33
nb-diar <diar@nusantarabetastudio.com>
2019-04-02 13:37:05 +0700

    set status on product status using const

--- modules/Bromo/Product/Models/ProductStatus.php
+++ modules/Bromo/Product/Models/ProductStatus.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ProductStatus extends Model
 {
     const SUBMIT = 1;
-    const APPROVE = 2;
-    const REJECT = 3;
+    const PUBLISH = 2;
+    const UNPUBLISH = 3;
+    const SUSPEND = 4;
+    const REJECT = 5;
 
     const CREATED_AT = null;
     const UPDATED_AT = null;
