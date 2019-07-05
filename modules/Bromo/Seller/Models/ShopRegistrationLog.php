1d5735f2209ea98fd4d6e2dab2792e3fbc6be585
Nb-Diar <diar@nusantarabetastudio.com>
2019-07-05 21:21:55 +0700

    add shop registration log model

---
+++ modules/Bromo/Seller/Models/ShopRegistrationLog.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bromo\Seller\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Nbs\BaseResource\Traits\SnowFlakeTrait;
+use stdClass;
+
+class ShopRegistrationLog extends Model
+{
+    use SnowFlakeTrait;
+
+    const CREATED_AT = null;
+    public $incrementing = false;
+    protected $table = 'shop_registration_log';
+    protected $fillable = [
+        'shop_id',
+        'shop_snapshot',
+        'status',
+        'notes',
+        'modified_by',
+        'modifier_role',
+    ];
+
+    public function setShopSnapshotAttribute($value)
+    {
+        $this->attributes['shop_snapshot'] = ($value) ? json_encode($value) : json_encode(new stdClass());
+    }
+}
