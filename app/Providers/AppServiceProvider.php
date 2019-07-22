5b05292c2fb6a3b339a1c08f86ff18313e32af70
Aditya <aditya@grosenia.co.id>
2019-07-22 12:02:16 +0000

    Feature/1114 force to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_SECURE', false)) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 
     /**
