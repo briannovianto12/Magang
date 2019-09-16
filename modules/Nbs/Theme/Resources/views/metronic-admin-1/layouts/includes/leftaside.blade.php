c642f26ec28e8948f007d4cb5f06a4a78056ef0e
rivaldi <rivaldi@grosenia.co.id>
2019-09-16 15:55:41 +0700

    feature(2280) Fixed Typo

--- modules/Nbs/Theme/Resources/views/metronic-admin-1/layouts/includes/leftaside.blade.php
+++ modules/Nbs/Theme/Resources/views/metronic-admin-1/layouts/includes/leftaside.blade.php
@@ -65,6 +65,11 @@
                     <span class="m-menu__link-text">Order</span>
                 </a>
             </li>
+            <li class="m-menu__item{{ nbs_helper()->isMenuActive('unverified.index') }}" aria-haspopup="true">
+                <a href="{{ route('unverified.index') }}" class="m-menu__link">
+                    <i class="m-menu__link-icon flaticon-avatar"></i>
+                    <span class="m-menu__link-text">Unverified Seller</span>
+                </a>
             <li class="m-menu__item{{ nbs_helper()->isMenuActive('messages.index') }}" aria-haspopup="true">
                 <a href="{{ route('messages.index') }}" class="m-menu__link">
                     <i class="m-menu__link-icon flaticon-open-box"></i>
