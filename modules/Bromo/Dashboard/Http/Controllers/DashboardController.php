cff365b12de3a9fb0118f129a7bb54e878c41795
Bryan <bryan@grosenia.co.id>
2019-09-25 04:25:50 +0000

    feature(2456) added sku statistics on admin dashboard

--- modules/Bromo/Dashboard/Http/Controllers/DashboardController.php
+++ modules/Bromo/Dashboard/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace Bromo\Dashboard\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Bromo\Product\Models\ProductStatus;
 use DB;
 
 class DashboardController extends Controller
@@ -26,6 +27,7 @@ class DashboardController extends Controller
            [ "name" => "Dashbor", "url" => route('dashboard') ]
         ];
         $data['summary'] = $this->getSummary();
+        
         return view('dashboard::index', $data);
         
     }
@@ -34,11 +36,17 @@ class DashboardController extends Controller
         $totalRegisteredSeller = DB::select("SELECT public.f_get_total_seller()");
         $totalRegisteredUser = DB::select("SELECT public.f_get_total_user()");
         $totalRegisteredSellerWithProduct = DB::select("SELECT public.f_get_sellers_with_total_product()");
+        $totalRegisteredSku = DB::table('product')->count();
+        $totalPublishedSku = DB::table('product')->where('status', ProductStatus::PUBLISH)->count();
+        $totalUnpublishedSku = DB::table('product')->where('status', '!=', ProductStatus::PUBLISH)->count();
         
         return [
             'total_registered_seller' => $totalRegisteredSeller[0]->f_get_total_seller,
             'total_registered_user' => $totalRegisteredUser[0]->f_get_total_user,
             'total_registered_seller_with_product' => $totalRegisteredSellerWithProduct[0]->f_get_sellers_with_total_product,
+            'total_registered_sku' => $totalRegisteredSku,
+            'total_published_sku' => $totalPublishedSku,
+            'total_unpublished_sku' => $totalUnpublishedSku,
         ];
 
 
