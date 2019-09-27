045e60a9096548d73c5c0382cb56a9527033006f
Rivaldi <rivaldi@grosenia.co.id>
2019-09-27 04:10:22 +0000

    Feature/2593 assign permission to order menu

--- modules/Bromo/Transaction/Resources/views/browse.blade.php
+++ modules/Bromo/Transaction/Resources/views/browse.blade.php
@@ -19,46 +19,163 @@
                 </div>
             </div>
         </div>
-        <div class="m-portlet__body">
-            <ul class="nav nav-tabs m-tabs-line m-tabs-line--info" role="tablist">
-                <li class="nav-item m-tabs__item">
-                    <a id="new_order_tab" class="nav-link m-tabs__link active" data-toggle="tab" href="#new_order"
-                       role="tab">
-                        <i class="la la-shopping-cart"></i> {{ __('Pesanan Dibuat') }}
-                    </a>
-                </li>
-                <li id="process_order_tab" class="nav-item m-tabs__item">
-                    <a class="nav-link m-tabs__link" data-toggle="tab" href="#process_order" role="tab">
-                        <i class="la la-hourglass-2"></i> {{ __('Pesanan Diproses') }}</a>
-                </li>
-                <li id="delivery_order_tab" class="nav-item m-tabs__item">
-                    <a class="nav-link m-tabs__link" data-toggle="tab" href="#delivery_order" role="tab">
-                        <i class="la la-truck"></i> {{ __('Pesanan Dikirim') }}</a>
-                </li>
-                <li id="delivered_order_tab" class="nav-item m-tabs__item">
-                    <a class="nav-link m-tabs__link" data-toggle="tab" href="#delivered_order" role="tab">
-                        <i class="la la-home"></i> {{ __('Sampai Tujuan') }}</a>
-                </li>
-                <li id="success_order_tab" class="nav-item m-tabs__item">
-                    <a class="nav-link m-tabs__link" data-toggle="tab" href="#success_order" role="tab">
-                        <i class="la la-check"></i> {{ __('Pesanan Sukses') }}</a>
-                </li>
-                <li id="cancel_order_tab" class="nav-item m-tabs__item">
-                    <a class="nav-link m-tabs__link" data-toggle="tab" href="#cancel_order" role="tab">
-                        <i class="la la-times"></i> {{ __('Pesanan Dibatalkan') }}</a>
-                </li>
-                <li id="list_order_tab" class="nav-item m-tabs__item">
-                    <a class="nav-link m-tabs__link" data-toggle="tab" href="#list_order" role="tab">
-                        <i class="la la-history"></i> {{ __('Transaksi') }}</a>
-                </li>
-            </ul>
-            <div class="tab-content">
-                <!--begin:: New Order Page-->
-                <div class="tab-pane active" id="new_order" role="tabpanel">
-                    <div class="table-responsive">
-                        <table id="table_new_order"
-                               class="table table-striped table-bordered table-hover display nowrap"
-                               style="width: 100%">
+        @can('view_order')
+            <div class="m-portlet__body">
+                <ul class="nav nav-tabs m-tabs-line m-tabs-line--info" role="tablist">
+                    <li class="nav-item m-tabs__item">
+                        <a id="new_order_tab" class="nav-link m-tabs__link active" data-toggle="tab" href="#new_order"
+                        role="tab">
+                            <i class="la la-shopping-cart"></i> {{ __('Pesanan Dibuat') }}
+                        </a>
+                    </li>
+                    <li id="process_order_tab" class="nav-item m-tabs__item">
+                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#process_order" role="tab">
+                            <i class="la la-hourglass-2"></i> {{ __('Pesanan Diproses') }}</a>
+                    </li>
+                    <li id="delivery_order_tab" class="nav-item m-tabs__item">
+                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#delivery_order" role="tab">
+                            <i class="la la-truck"></i> {{ __('Pesanan Dikirim') }}</a>
+                    </li>
+                    <li id="delivered_order_tab" class="nav-item m-tabs__item">
+                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#delivered_order" role="tab">
+                            <i class="la la-home"></i> {{ __('Sampai Tujuan') }}</a>
+                    </li>
+                    <li id="success_order_tab" class="nav-item m-tabs__item">
+                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#success_order" role="tab">
+                            <i class="la la-check"></i> {{ __('Pesanan Sukses') }}</a>
+                    </li>
+                    <li id="cancel_order_tab" class="nav-item m-tabs__item">
+                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#cancel_order" role="tab">
+                            <i class="la la-times"></i> {{ __('Pesanan Dibatalkan') }}</a>
+                    </li>
+                    <li id="list_order_tab" class="nav-item m-tabs__item">
+                        <a class="nav-link m-tabs__link" data-toggle="tab" href="#list_order" role="tab">
+                            <i class="la la-history"></i> {{ __('Transaksi') }}</a>
+                    </li>
+                </ul>
+                <div class="tab-content">
+                    <!--begin:: New Order Page-->
+                    <div class="tab-pane active" id="new_order" role="tabpanel">
+                        <div class="table-responsive">
+                            <table id="table_new_order"
+                                class="table table-striped table-bordered table-hover display nowrap"
+                                style="width: 100%">
+                                <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Code</th>
+                                    <th>Buyer</th>
+                                    <th>Seller</th>
+                                    <th>Payment Method</th>
+                                    <th>Amount</th>
+                                    <th>Notes</th>
+                                    <th>Status</th>
+                                    <th>Date</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                    <!--end:: New Order Page-->
+
+                    <!--begin:: Process Order Page-->
+                    <div class="tab-pane" id="process_order" role="tabpanel">
+                        <table id="table_process_order"
+                            class="table table-striped table-bordered table-hover display nowrap"
+                            style="width: 100%">
+                            <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Code</th>
+                                <th>Buyer</th>
+                                <th>Seller</th>
+                                <th>Payment Method</th>
+                                <th>Amount</th>
+                                <th>Notes</th>
+                                <th>Status</th>
+                                <th>Updated Date</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <!--end:: Process Order Page-->
+
+                    <!--begin:: Delivery Order Page-->
+                    <div class="tab-pane" id="delivery_order" role="tabpanel">
+                        <table id="table_delivery_order"
+                            class="table table-striped table-bordered table-hover display nowrap"
+                            style="width: 100%">
+                            <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Code</th>
+                                <th>Buyer</th>
+                                <th>Seller</th>
+                                <th>Payment Method</th>
+                                <th>Amount</th>
+                                <th>Notes</th>
+                                <th>Status</th>
+                                <th>Updated Date</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <!--end:: Delivery Order Page-->
+
+                    <!--begin:: Delivered Order Page-->
+                    <div class="tab-pane" id="delivered_order" role="tabpanel">
+                        <table id="table_delivered_order"
+                            class="table table-striped table-bordered table-hover display nowrap"
+                            style="width: 100%">
+                            <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Code</th>
+                                <th>Buyer</th>
+                                <th>Seller</th>
+                                <th>Payment Method</th>
+                                <th>Amount</th>
+                                <th>Notes</th>
+                                <th>Status</th>
+                                <th>Updated Date</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <!--end:: Delivered Order Page-->
+
+                    <!--begin:: Success Order Page-->
+                    <div class="tab-pane" id="success_order" role="tabpanel">
+                        <table id="table_success_order"
+                            class="table table-striped table-bordered table-hover display nowrap"
+                            style="width: 100%">
+                            <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Code</th>
+                                <th>Buyer</th>
+                                <th>Seller</th>
+                                <th>Payment Method</th>
+                                <th>Amount</th>
+                                <th>Notes</th>
+                                <th>Status</th>
+                                <th>Updated Date</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <!--end:: Success Order Page-->
+
+                    <!--begin:: Cancel Order Page-->
+                    <div class="tab-pane" id="cancel_order" role="tabpanel">
+                        <table id="table_cancel_order"
+                            class="table table-striped table-bordered table-hover display nowrap"
+                            style="width: 100%">
                             <thead>
                             <tr>
                                 <th>No</th>
@@ -75,148 +192,33 @@
                             </thead>
                         </table>
                     </div>
-                </div>
-                <!--end:: New Order Page-->
+                    <!--end:: Cancel Order Page-->
 
-                <!--begin:: Process Order Page-->
-                <div class="tab-pane" id="process_order" role="tabpanel">
-                    <table id="table_process_order"
-                           class="table table-striped table-bordered table-hover display nowrap"
-                           style="width: 100%">
-                        <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Code</th>
-                            <th>Buyer</th>
-                            <th>Seller</th>
-                            <th>Payment Method</th>
-                            <th>Amount</th>
-                            <th>Notes</th>
-                            <th>Status</th>
-                            <th>Updated Date</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                    </table>
+                    <!--begin:: List Order Page-->
+                    <div class="tab-pane" id="list_order" role="tabpanel">
+                        <table id="table_list_order"
+                            class="table table-striped table-bordered table-hover display nowrap"
+                            style="width: 100%">
+                            <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Code</th>
+                                <th>Buyer</th>
+                                <th>Seller</th>
+                                <th>Payment Method</th>
+                                <th>Amount</th>
+                                <th>Notes</th>
+                                <th>Status</th>
+                                <th>Date</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <!--end:: List Order Page-->
                 </div>
-                <!--end:: Process Order Page-->
-
-                <!--begin:: Delivery Order Page-->
-                <div class="tab-pane" id="delivery_order" role="tabpanel">
-                    <table id="table_delivery_order"
-                           class="table table-striped table-bordered table-hover display nowrap"
-                           style="width: 100%">
-                        <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Code</th>
-                            <th>Buyer</th>
-                            <th>Seller</th>
-                            <th>Payment Method</th>
-                            <th>Amount</th>
-                            <th>Notes</th>
-                            <th>Status</th>
-                            <th>Updated Date</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                    </table>
-                </div>
-                <!--end:: Delivery Order Page-->
-
-                <!--begin:: Delivered Order Page-->
-                <div class="tab-pane" id="delivered_order" role="tabpanel">
-                    <table id="table_delivered_order"
-                           class="table table-striped table-bordered table-hover display nowrap"
-                           style="width: 100%">
-                        <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Code</th>
-                            <th>Buyer</th>
-                            <th>Seller</th>
-                            <th>Payment Method</th>
-                            <th>Amount</th>
-                            <th>Notes</th>
-                            <th>Status</th>
-                            <th>Updated Date</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                    </table>
-                </div>
-                <!--end:: Delivered Order Page-->
-
-                <!--begin:: Success Order Page-->
-                <div class="tab-pane" id="success_order" role="tabpanel">
-                    <table id="table_success_order"
-                           class="table table-striped table-bordered table-hover display nowrap"
-                           style="width: 100%">
-                        <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Code</th>
-                            <th>Buyer</th>
-                            <th>Seller</th>
-                            <th>Payment Method</th>
-                            <th>Amount</th>
-                            <th>Notes</th>
-                            <th>Status</th>
-                            <th>Updated Date</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                    </table>
-                </div>
-                <!--end:: Success Order Page-->
-
-                <!--begin:: Cancel Order Page-->
-                <div class="tab-pane" id="cancel_order" role="tabpanel">
-                    <table id="table_cancel_order"
-                           class="table table-striped table-bordered table-hover display nowrap"
-                           style="width: 100%">
-                        <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Code</th>
-                            <th>Buyer</th>
-                            <th>Seller</th>
-                            <th>Payment Method</th>
-                            <th>Amount</th>
-                            <th>Notes</th>
-                            <th>Status</th>
-                            <th>Date</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                    </table>
-                </div>
-                <!--end:: Cancel Order Page-->
-
-                <!--begin:: List Order Page-->
-                <div class="tab-pane" id="list_order" role="tabpanel">
-                    <table id="table_list_order"
-                           class="table table-striped table-bordered table-hover display nowrap"
-                           style="width: 100%">
-                        <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Code</th>
-                            <th>Buyer</th>
-                            <th>Seller</th>
-                            <th>Payment Method</th>
-                            <th>Amount</th>
-                            <th>Notes</th>
-                            <th>Status</th>
-                            <th>Date</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                    </table>
-                </div>
-                <!--end:: List Order Page-->
             </div>
-        </div>
+        @endcan
     </div>
 
 @endsection
