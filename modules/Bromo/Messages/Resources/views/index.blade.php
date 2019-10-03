8d6819ba85d58d84bcde1647dd75d2e3d9cdb5b5
rivaldi <rivaldi@grosenia.co.id>
2019-10-03 11:20:52 +0700

    feature(2734) Assign Permission to Messages Menu

--- modules/Bromo/Messages/Resources/views/index.blade.php
+++ modules/Bromo/Messages/Resources/views/index.blade.php
@@ -12,45 +12,45 @@
           'portlet_head' => true,
           'portlet_title' => "List {$title}"])
         @slot('body')
-            
-            {{ $results->links() }}
-            <table class="table">
-                <thead>
-                    <th>Sender</th>
-                    <th>Receiver</th>
-                    <th>Message</th>
-                    <th>Created At</th>
-                </thead>
-                <tbody>
-                    @foreach($results as $item)
-                        <tr>
-                            <td>
-                              <strong>{{ $item->sender }}</strong><br/>
-                              {{ $item->sender_name }} <br/>
-                              @isset($item->sender_shop_name)
-                                <a>shop name: <strong>{{$item->sender_shop_name}}</strong> </a><br/>
-                              @endisset
-                              {{ $item->sender_phone }}
-                            </td>
-                            <td>
-                              <strong>{{ $item->receiver }}</strong><br/>
-                              {{ $item->receiver_name }} <br/>
-                              @isset($item->receiver_shop_name)
-                                <a>shop name: <strong>{{$item->receiver_shop_name}}</strong> </a><br/>
-                              @endisset
-                              {{ $item->receiver_phone }}
-                            </td>
-                            <td>
-                             {!! $item->message !!}
-                            </td>
-                            <td>
-                            {!! $item->time !!}
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
+            @can('view_messages')
+              {{ $results->links() }}
+              <table class="table">
+                  <thead>
+                      <th>Sender</th>
+                      <th>Receiver</th>
+                      <th>Message</th>
+                      <th>Created At</th>
+                  </thead>
+                  <tbody>
+                      @foreach($results as $item)
+                          <tr>
+                              <td>
+                                <strong>{{ $item->sender }}</strong><br/>
+                                {{ $item->sender_name }} <br/>
+                                @isset($item->sender_shop_name)
+                                  <a>shop name: <strong>{{$item->sender_shop_name}}</strong> </a><br/>
+                                @endisset
+                                {{ $item->sender_phone }}
+                              </td>
+                              <td>
+                                <strong>{{ $item->receiver }}</strong><br/>
+                                {{ $item->receiver_name }} <br/>
+                                @isset($item->receiver_shop_name)
+                                  <a>shop name: <strong>{{$item->receiver_shop_name}}</strong> </a><br/>
+                                @endisset
+                                {{ $item->receiver_phone }}
+                              </td>
+                              <td>
+                              {!! $item->message !!}
+                              </td>
+                              <td>
+                              {!! $item->time !!}
+                              </td>
+                          </tr>
+                      @endforeach
+                  </tbody>
+              </table>
+            @endcan
         @endslot
 
         @slot('postfix')
