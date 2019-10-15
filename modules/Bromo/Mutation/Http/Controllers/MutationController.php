3900afb79db510f78ab6d9011af0f1971783debb
rivaldi <rivaldi@grosenia.co.id>
2019-10-15 15:19:55 +0700

    feature(2868) Create Shop Log Mutation

--- modules/Bromo/Mutation/Http/Controllers/MutationController.php
+++ modules/Bromo/Mutation/Http/Controllers/MutationController.php
@@ -35,6 +35,7 @@ class MutationController extends Controller
             \Log::debug($data);
             return datatables()->of($data)->make(true);
         }
+        // \Log::debug($data);
         return view('mutation::index');
     }
 }
