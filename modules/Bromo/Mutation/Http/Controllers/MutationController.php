24eef4b7dec2cbf34ea10237de1d4e269f0f4cc1
rivaldi <rivaldi@grosenia.co.id>
2019-10-21 15:15:20 +0700

    feat(2868) Remove "Shop ID", Add "Shop Name" and "Owner Name"

--- modules/Bromo/Mutation/Http/Controllers/MutationController.php
+++ modules/Bromo/Mutation/Http/Controllers/MutationController.php
@@ -15,11 +15,19 @@ class MutationController extends Controller
             if(!empty($request->from_date)){
                 \Log::debug($request);
                 $data = DB::table('shop_log_mutation')
+                ->join('shop','shop_log_mutation.shop_id', 'shop.id')
                 ->whereBetween('created_at', array($request->from_date, $request->to_date))
+                ->select('shop_log_mutation.*', 'shop.name')
                 ->get();
                 \Log::debug($data);
             }else{
                 $data = DB::table('shop_log_mutation')
+                ->join('shop','shop_log_mutation.shop_id', 'shop.id')
+                ->join('business','business.id', 'shop.business_id')
+                ->join('business_member','business.id', 'business_member.business_id')
+                ->join('user_profile','user_profile.id', 'business_member.user_id')
+                ->where('business_member.role', '1')
+                ->select('shop_log_mutation.*', 'shop.name', 'user_profile.full_name')
                 ->get();
             }
             \Log::debug($data);
