e09afb3bd3e4db04bec668c8a34a3647834fc06f
rivaldigunarso <rivaldi@grosenia.co.id>
2019-10-23 13:05:03 +0700

    bugfix(3125) Fix Shop Log Mutation Date Range

--- modules/Bromo/Mutation/Http/Controllers/MutationController.php
+++ modules/Bromo/Mutation/Http/Controllers/MutationController.php
@@ -15,8 +15,13 @@ class MutationController extends Controller
             if(!empty($request->from_date)){
                 $data = DB::table('shop_log_mutation')
                 ->join('shop','shop_log_mutation.shop_id', 'shop.id')
-                ->whereBetween('created_at', array($request->from_date, $request->to_date))
-                ->select('shop_log_mutation.*', 'shop.name')
+                ->join('business','business.id', 'shop.business_id')
+                ->join('business_member','business.id', 'business_member.business_id')
+                ->join('user_profile','user_profile.id', 'business_member.user_id')
+                ->whereBetween('shop_log_mutation.created_at', array($request->from_date, $request->to_date))
+                ->select('shop_log_mutation.id', 'shop_log_mutation.shop_id', 'shop_log_mutation.mutation',
+                        'shop_log_mutation.remark', 'shop_log_mutation.trx_type', 'shop_log_mutation.created_at',
+                        'shop_log_mutation.updated_at', 'shop.name', 'user_profile.full_name')
                 ->get();
             }else{
                 $data = DB::table('shop_log_mutation')
