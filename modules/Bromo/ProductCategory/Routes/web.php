1e1da3310ddadbda38d7d79687ad9c1cc7758e9e
rivaldigunarso <rivaldi@grosenia.co.id>
2019-10-24 10:36:51 +0700

    feat(3135) Hide Route for Each Actions

--- modules/Bromo/ProductCategory/Routes/web.php
+++ modules/Bromo/ProductCategory/Routes/web.php
@@ -17,16 +17,16 @@ Route::prefix('product-category')->name('product-category')->group(function () {
 
     Route::get('/', 'ProductCategoryController@index');
     // Attributes
-    Route::get('/{product_category}/attributes', 'ProductCategoryController@attributes')->name('.attributes');
-    Route::post('/{product_category}/attributes/{id}', 'ProductCategoryController@attachAttribute')
-        ->name('.attributes.attach');
-    Route::delete('/{product_category}/attributes/{id}', 'ProductCategoryController@detachAttribute')
-        ->name('.attributes.detach');
+    // Route::get('/{product_category}/attributes', 'ProductCategoryController@attributes')->name('.attributes');
+    // Route::post('/{product_category}/attributes/{id}', 'ProductCategoryController@attachAttribute')
+    //     ->name('.attributes.attach');
+    // Route::delete('/{product_category}/attributes/{id}', 'ProductCategoryController@detachAttribute')
+    //     ->name('.attributes.detach');
     // Brands
-    Route::get('/{product_category}/brands', 'ProductCategoryController@brands')->name('.brands');
-    Route::post('/{product_category}/brands/{id}', 'ProductCategoryController@attachBrand')
-        ->name('.brands.attach');
-    Route::delete('/{product_category}/brands/{id}', 'ProductCategoryController@detachBrand')
-        ->name('.brands.detach');
+    // Route::get('/{product_category}/brands', 'ProductCategoryController@brands')->name('.brands');
+    // Route::post('/{product_category}/brands/{id}', 'ProductCategoryController@attachBrand')
+    //     ->name('.brands.attach');
+    // Route::delete('/{product_category}/brands/{id}', 'ProductCategoryController@detachBrand')
+    //     ->name('.brands.detach');
 
 });
