071772f4a9253f76c7fd5a2c55bbe0281b0facac
geraldi.grosenia <geraldi@grosenia.co.id>
2020-04-17 17:06:20 +0700

    bugfix(ADM-95) Update helper

--- modules/Bromo/Transaction/Helpers/XenditBankNameUtil.php
+++ modules/Bromo/Transaction/Helpers/XenditBankNameUtil.php
@@ -2,46 +2,26 @@
 namespace Bromo\Transaction\Helpers;
 
 class XenditBankNameUtil {
-
-    //Unique Code Length
-    const minLength = 4;
-    const maxLength = 5;
-
-    //Development
-    const DEV = [
-        '8808' => 'BNI',
-        '10766' => 'BCA',
-        '26215' => 'BRI',
-        '88608' => 'MANDIRI',
-    ];
-
-    //Production
-    const PROD = [
-        '8808' => 'BNI',
-        '11687' => 'BCA',
-        '26215' => 'BRI',
-        '88608' => 'MANDIRI',
-    ];
     
     /**
      * Get Bank Name
      */
     public static function getBankName ( $bankAccountNumber ) {
-        //CHECK ENVIRONMENT
+        //Unique Code Length
+        $minLength = 4;
+        $maxLength = 5;
+
+        //Bank Name Collection
         $bankNameCollection = [];
-        switch(config('app.env')){
-            case 'production':
-                $bankNameCollection = self::PROD;
-                break;
-            case 'development':
-                $bankNameCollection = self::DEV;
-                break;
-        }
+        if(config('shop.prefix_va_bni')) $bankNameCollection[config('shop.prefix_va_bni')] = 'BNI';
+        if(config('shop.prefix_va_bca')) $bankNameCollection[config('shop.prefix_va_bca')] = 'BCA';
+        if(config('shop.prefix_va_bri')) $bankNameCollection[config('shop.prefix_va_bri')] = 'BRI';
+        if(config('shop.prefix_va_mandiri')) $bankNameCollection[config('shop.prefix_va_mandiri')] = 'MANDIRI';
 
         //FIND BANK NAME (LONGER CODE == MORE SPECIFIC)
         $bankName = null;
         if($bankNameCollection != []){
-            for($codeLength = self::minLength; $codeLength <= self::maxLength; $codeLength++){
+            for($codeLength = $minLength; $codeLength <= $maxLength; $codeLength++){
                 $bankCode = substr($bankAccountNumber,0,$codeLength);
                 if(array_key_exists($bankCode, $bankNameCollection)){
                     $bankName = $bankNameCollection[$bankCode];
