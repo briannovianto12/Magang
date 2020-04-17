97bcd864e1ced64a5fcd7170aec2dcda7dff6800
Aditya <aditya@grosenia.co.id>
2020-04-17 07:41:28 +0000

    Bugfix/adm 96 blacklist user

--- modules/Bromo/Tools/Resources/views/blacklist-phone-number.blade.php
+++ modules/Bromo/Tools/Resources/views/blacklist-phone-number.blade.php
@@ -1,41 +1,50 @@
 @extends('theme::layouts.master')
 
 @section('css')
-    <link href="{{ nbs_asset('vendors/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="{{ nbs_asset('vendor/fancybox/jquery.fancybox.css') }}">
+    <style>
+        #bromo .m-widget5 .m-widget5__item .m-widget5__content:last-child {
+            float: none;
+        }
+
+        #bromo .list-inline-item:not(:last-child) {
+            margin-right: 5rem;
+            vertical-align: top;
+        }
+    </style>
 @endsection
 
 @section('scripts')
-    <script src="{{ nbs_asset('vendors/custom/datatables/datatables.bundle.js') }}"></script>
+    <script src="{{ nbs_asset('vendor/fancybox/jquery.fancybox.js') }}"></script>
 @endsection
 
 @section('content')
-    @can('view_postal_code_finder')
-    <div class="m-portlet m-portlet--mobile">
-        <div class="m-portlet__head">
-            <div class="m-portlet__head-caption">
-                <div class="m-portlet__head-title">
-                    <h3 class="m-portlet__head-text">
-                        Blacklist Phone Number
-                    </h3>
+    @component('components._portlet',[
+          'portlet_head' => true,
+          'portlet_title' => "Blacklist Phone Number"])
+        @slot('body')
+            @can('view_blacklist_user')
+            <div class="row">
+                <div class="col-3">
                 </div>
-            </div>
-        </div>
-        <div class="m-portlet__body">
-           <div class="row">
-                <div class="col-6" id="result-container">
-          
+                <div class="col-6">
                     <form action="{{ route('blacklist-phone-number.post-table') }}" method="POST">
-                            {{ csrf_field() }}
+                        {{ csrf_field() }}
                         <div class="form-group">
                             <label for="banner-title">Phone Number</label>
-                            <input id="postal-code" class="form-control" type="text" name="msisdn" placeholder="Ex : 628123456789" >
+                            <input id="postal-code" class="form-control" type="text" name="msisdn" placeholder="Ex : +628123456789" >
                         </div>
                             <button type="submit" class="btn btn-primary btn-block" style="border-radius: 6px">Submit</button>
                     </form>
                 </div>
             </div>
-        </div>
-    </div>
-    @endcan
+            @endcan
+        @endslot
+
+        @slot('postfix')
+            Blacklist Phone Number
+        @endslot
+    @endcomponent
+
 @endsection
 
