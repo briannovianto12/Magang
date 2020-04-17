a38809c199d2129daf25addbd8e1355343799ca0
Hikvineh <aditya@aditya.aditya>
2020-04-17 13:21:22 +0700

    feature(ADM-101-perbaikan-change-shop-description) add character remainig

--- modules/Bromo/Seller/Resources/views/js.blade.php
+++ modules/Bromo/Seller/Resources/views/js.blade.php
@@ -172,4 +172,12 @@
             }
         });
     }
+
+    var maxLength = 128;
+    
+    $('textarea').keyup(function() {
+      var length = $(this).val().length;
+      var length = maxLength-length;
+      $('#chars').text(length);
+    });
 </script>
