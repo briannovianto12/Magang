d4a45f1ee7e1c61ae3e07ff462d4d366c4680f67
Geraldi Grosenia <geraldi@grosenia.co.id>
2020-05-07 23:58:41 +0000

    feat(ADM-93) Preserve list state on order page

--- modules/Bromo/Transaction/Config/config.php
+++ modules/Bromo/Transaction/Config/config.php
@@ -6,5 +6,6 @@ return [
         'logo' => 'businesses/logos/',
         'buyer_avatar' => 'buyers/avatars/'
     ],
-    'get_tracking_id_sleep' => env('TRACKING_ID_SLEEP', 3)
+    'get_tracking_id_sleep' => env('TRACKING_ID_SLEEP', 3),
+    'datatable_session_duration' => env('DATATABLE_SESSION_DURATION',3600)
 ];
